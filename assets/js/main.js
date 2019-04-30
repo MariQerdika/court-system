78cb6214f4a8d3621c8d81fcec18a5dc201eb870
MariQerdika <40671485+MariQerdika@users.noreply.github.com>
2019-04-30 20:34:58 +0400

    add popup and add input fields

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,73 @@ $(document).ready(function () {
         $(".active_tab").not(this).removeClass("active_class");
     })
     //  end tab style
+
+    function mounted() {
+        $('#select1').multipleSelect()
+        $('#select2').multipleSelect()
+        $('#select3').multipleSelect()
+        $('#select4').multipleSelect()
+        $('#select5').multipleSelect()
+    }
+    mounted();
+
+
+
+    //Add Input Fields
+    var max_fields = 5;
+    var wrapper = $(".wrapper");
+    var add_button = $(".add_fields");
+    var x = 1;
+    $(add_button).click(function (e) {
+        e.preventDefault();
+
+        if (x < max_fields) {
+            x++;
+            $(wrapper).append('<div class="addinput"><input class="form-control" type="text" name="input_array_name[]" placeholder="შეიყვანეთ ფრაგმენტი ტექსტიდან" /> <a href="javascript:void(0);" class="remove_field"><img src="assets/images/error.png"> </a></div>');
+        }
+        if (x === 5) {
+            document.getElementById("InputHide").style.display = "none";
+        }
+    });
+    $(wrapper).on("click", ".remove_field", function (e) {
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
 });
 
-// add infut fields
-function addInput() {
-    var i = 1;
-    if ($(".exampler").length < 4) my_div.innerHTML = my_div.innerHTML + "<input type='text' class='form-control exampler' placeholder='შეიყვანეთ ფრაგმენტი ტექსტიდან' name='mytext'+ i>"
+
+
+
+
+
+
+
+
+
+
+
+
+// modal
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.querySelectorAll('.modalBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+for (i = 0; i < modalBtn.length; i++) {
+    modalBtn[i].addEventListener('click', openModal);
 }
+
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+function openModal() {
+    modal.style.display = 'block';
+}
+function closeModal() {
+    modal.style.display = 'none';
+}
+function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
